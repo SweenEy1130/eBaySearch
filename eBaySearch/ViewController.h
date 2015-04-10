a358938507bb15c09dfe926ada97ee876e844cc3
SweenEy1130 <ronnie.alonso@gmail.com>
2015-04-09 19:03:49 -0700

    Initial Commit

---
+++ eBaySearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eBaySearch
+//
+//  Created by Zheng Li on 4/9/15.
+//  Copyright (c) 2015 Zheng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ eBaySearchTests/eBaySearchTests.m
@@ -0,0 +1,40 @@
+//
+//  eBaySearchTests.m
+//  eBaySearchTests
+//
+//  Created by Zheng Li on 4/9/15.
+//  Copyright (c) 2015 Zheng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface eBaySearchTests : XCTestCase
+
+@end
+
+@implementation eBaySearchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
